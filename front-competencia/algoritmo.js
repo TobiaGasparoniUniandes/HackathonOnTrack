dbad2ea4c54815b55c5073fc55493cea1ef20415
TobiaGasparoniUniandes <tr.gasparoni@uniandes.edu.co>
2019-11-08 19:07:41 -0500

    fucking works

--- front-competencia/algoritmo.js
+++ front-competencia/algoritmo.js
@@ -1,116 +1,63 @@
-var lista;
-var listaYaPaso;
-var listafalta;
-var listo;
+var partida = [{location: 'Go Green, Aguas, Bogota'}];
+var colegio = [{location: 'Colegio Italiano Leonardo Da Vinci, Bogota'}];
+var paradas = [
+	{location:"Parque Nacional Enrique Olaya Herrera, Bogota"},
+	{location:"Parque Gernika, Bogota"},
+	{location:"Basílica Menor Nuestra Señora de Lourdes, Bogota"},
+	{location:"Carulla Calle 102, Bogota"}
+];
+var respuestaAPI;
 
-listo = [{ location: 'Calle 85 #23-10, Bogota' },
-{ location: 'Calle 93 #19-10, Bogota' },
-{ location: 'Calle 100 #19-10, Bogota' },
-{ location: 'Calle 108 #15-10, Bogota' },
-{ location: 'Calle 116 #20-10, Bogota' }];
+function totDist()
+{
+	var dist = 0;
+	
+	for(i = 0; i < respuestaAPI.routes[0].legs.length; i++)
+	{
+		dist += respuestaAPI.routes[0].legs[i].distance.value;
+	}
+	
+	return dist;
+}
 
-listafalta = listo;
+function distParcial(n)
+{
+	var dist = 0;
+	
+	for(i = 0; i < n+1; i++)
+	{
+		dist += respuestaAPI.routes[0].legs[i].distance.value;
+	}
+	var response = 'La distancia desde el parqueadero hasta ' + respuestaAPI.routes[0].legs[n].end_address + ' es ' + (dist/1000) + 'km';
+	
+	return response;
+}
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 4.66421, lng: -74.07861 },
-        zoom: 16
-    });
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 4.66421, lng: -74.07861},
+		zoom: 16}
+	)
 
-    inicializarLista();
-}
+	var directionsService = new google.maps.DirectionsService();
+	//DirectionsRenderer initialized
 
-function inicializarLista() {
-    var directionsService = new google.maps.DirectionsService();
-    var directionsRenderer = new google.maps.DirectionsRenderer({
-        supressMarkers: true
-    })
+	let request = {
+		origin: partida[0].location,
+		destination: colegio[0].location,
+		travelMode: 'DRIVING',
+		waypoints: paradas
+	};
 
-    let request = {
-        origin: 'Colegio Italiano Leonardo Da Vinci, Bogota',
-        destination: 'Colegio Italiano Leonardo Da Vinci, Bogota',
-        travelMode: 'DRIVING',
-        waypoints: [{ location: 'Calle 85 #23-10, Bogota' },
-        { location: 'Calle 93 #19-10, Bogota' },
-        { location: 'Calle 100 #19-10, Bogota' },
-        { location: 'Calle 108 #15-10, Bogota' },
-        { location: 'Calle 116 #20-10, Bogota' }]
-    };
-
-    directionsService.route(request, function (result, status) {
-        console.log(result);
-        lista = result;
-        if (status == 'OK') {
-            directionsRenderer.setDirections(result);
-        }
-    });
-    directionsRenderer.setMap(map);
-}
-
-
-function ya(b) {
-    for (i = 0; i < b; i++) {
-        listaYaPaso[i] = listo[i];
-        listafalta.shift();
-
-    }
-}
-
-
-function calcularPorcentaje(puntoActual, puntoFinalDefinido) {
-    return (distanciaRecorrida(puntoActual) / distanciaTotal()) * 100;
-}
-
-
-function distanciaRecorrida(puntoActual) {
-    dist = 0;
-    for (i = 0; i < listaYaPaso.routes[0].legs.length; i++) {
-        dist = dist + lista.routes[0].legs[i].distance.value;
-
-    }
-    return dist
-}
-
-function distanciaTotal() {
-    dist = 0;
-    for (i = 0; i < lista.routes[0].legs.length; i++) {
-        dist = dist + lista.routes[0].legs[i].distance.value;
-    }
-    return dist
-}
-
-
-/*
-*BONO
-*/
-/* dentro = false;
-function estaDentroCamino(punto)
-{
-    if(){
-        dentro = true;
-    }
-    return dentro
-}
- */
-
-/***
- * Modifica Json (BONO)
- */
-function calcularNuevaRuta(puntoActual) {
-
-
-    let request = {
-        origin: puntoActual,
-        destination: 'Colegio Italiano Leonardo Da Vinci, Bogota',
-        travelMode: 'DRIVING',
-        waypoints: listafalta
-    };
-
-    directionsService.route(request, function (result, status) {
-        console.log(result);
-        if (status == 'OK') {
-            directionsRenderer.setDirections(result);
-        }
-    });
-    directionsRenderer.setMap(map);
-}
+	directionsService.route(request, function(result, status) {
+		console.log(result);
+		respuestaAPI = result;
+		console.log(totDist());
+		console.log(distParcial(0));
+		if(status == 'OK')
+		{
+			//directionsRenderer.setDirections(result);
+		}
+		}
+	)
+};
